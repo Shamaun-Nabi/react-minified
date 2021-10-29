cda13d80e8bcc3818e34e5480aa674a4ee4534f6
Shamaun-Nabi <snashik950@gmail.com>
2021-10-30 00:19:07 +0600

    messeage

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
       <Anime />
       <Anime />
       <Anime />
-      <h3 className="text-center text-green-400">
-        Booyah !! React minified(tailwind css and Bootsrap Included)
+      <h3 className="text-center text-green-400 bg-gray-200 p-4 shadow-md">
+        Booyah !! React minified(tailwind css and Bootstrap Included)
+        <p className="font-thin text-md text-pink-700">Shamaun Nabi</p>
       </h3>
+      
       <Anime />
       <Anime />
       <Anime />
