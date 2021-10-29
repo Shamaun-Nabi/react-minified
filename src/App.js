0672c0d75b1311e2b96333a23268128c563bbc7b
Shamaun-Nabi <snashik950@gmail.com>
2021-10-30 00:15:46 +0600

    tailwind added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ function App() {
   return (
     // Remove this area and start your code
     <div className="container">
-        <Anime/>
-        <Anime/>
-        <Anime/>
-        <Anime/>
-        <Anime/>
-        <Anime/>
-      <h3 className="text-center">
-       My app
+      <Anime />
+      <Anime />
+      <Anime />
+      <Anime />
+      <Anime />
+      <Anime />
+      <h3 className="text-center text-green-400">
+        Booyah !! React minified(tailwind css and Bootsrap Included)
       </h3>
-      <Anime/>
-      <Anime/>
-      <Anime/>
-      <Anime/>
-      <Anime/>
-      <Anime/>
+      <Anime />
+      <Anime />
+      <Anime />
+      <Anime />
+      <Anime />
+      <Anime />
     </div>
     // Remove this area and start your code
   );
