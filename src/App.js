06c029261c12b502f6a476280fd2b5d0fc7736c2
Shamaun-Nabi <snashik950@gmail.com>
2021-10-29 16:35:15 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h3 className="text-center">
+        Minified By Shamaun Nabi with Bootstrap included
+      </h3>
+    </div>
+  );
+}
+
+export default App;
