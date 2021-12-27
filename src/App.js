c5e7e1f21d020914d53bc13ca52147d4174fcc5f
Shamaun-Nabi <snashik950@gmail.com>
2021-12-27 14:37:37 +0600

    dlt unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Anime from "./components/Anime";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 // import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -8,17 +7,7 @@ function App() {
   return (
     // Remove this area and start your code
     <div className="container">
-      <Anime />
-      <h3 className="text-center text-green-400 bg-gray-200 p-4 shadow-md">
-        Booyah !! React minified(tailwind css and Bootstrap Included)
-        <p className="font-thin text-md text-pink-700">
-           Shamaun Nabi
-        </p>
-        <p><FontAwesomeIcon icon={faCoffee} spin/> FontAwesomeIcon Added</p>
-      </h3>
-
-      <Anime />
-    
+      <h3>Hello Nabi's World</h3>
     </div>
     // Remove this area and start your code
   );
